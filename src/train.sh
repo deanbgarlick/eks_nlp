a72af20b5ca11f7b56646e6bad2b8e279c7302c4
deanbgarlick <deanbgarlick@gmail.com>
2021-10-15 22:48:17 -0700

    Add saving of binaries to s3

--- src/train.sh
+++ src/train.sh
@@ -2,3 +2,9 @@ kaggle competitions download -c nlp-getting-started
 unzip nlp-getting-started.zip -d data
 
 python -m train
+
+aws s3 mb s3://disaster-tweets-example-remote-storage
+aws s3 cp binaries s3://disaster-tweets-example-remote-storage/binaries --recursive
+
+rm -rf binaries/tokenizer
+rm -rf binaries/model
